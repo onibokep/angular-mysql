dcc93b8fa7ceef4513bc28c7607a250967300e44
onibokep <47023943+onibokep@users.noreply.github.com>
2019-08-01 13:44:12 +0700

    Add files via upload

---
+++ client/src/app/components/game-list/game-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+import { GamesService } from '../../services/games.service';
+import { Game } from '../../models/Game';
+
+@Component({
+  selector: 'app-game-list',
+  templateUrl: './game-list.component.html',
+  styleUrls: ['./game-list.component.css']
+})
+export class GameListComponent implements OnInit {
+
+  @HostBinding('class') classes = 'row';
+
+  games: any = [];
+
+  constructor(private gamesService: GamesService) { }
+
+  ngOnInit() {
+    this.getGames();
+  }
+
+  getGames() {
+    this.gamesService.getGames()
+    .subscribe(
+      res => {
+        this.games = res;
+      },
+      err => console.log(err)
+    );
+  }
+
+  deleteGame(id: string) {
+    this.gamesService.deleteGame(id)
+    .subscribe(
+      res => {
+        console.log(res);
+        this.getGames();
+      },
+      err => {
+        console.log(err);
+      }
+    )
+  }
+}
